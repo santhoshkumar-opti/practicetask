c24500cd8e6ec2f5ce1a93fff730e936fad98f69
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-12 20:29:19 +0530

    feat: done static images

---
+++ navsection/script.js
@@ -0,0 +1,22 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Get all menu items
+//   const shopItem = document.querySelector("#shop-desktop");
+//   const subMenu = document.querySelector("#shop-desktop-dropdown");
+
+//   subMenu.style.width = `${window.innerWidth}px`;
+
+//   // Add event listeners to show sub-menu on mouseover
+//   shopItem.addEventListener("mouseover", function () {
+//     console.dir(this)
+//     if (subMenu) {
+//       subMenu.style.display = "block";
+//     }
+//   });
+//   // Add event listener to hide sub-menu on mouseout
+//   shopItem.addEventListener("mouseout", function () {
+//     if (subMenu) {
+//       subMenu.style.display = "none";
+//     }
+//   });
+
+// });
