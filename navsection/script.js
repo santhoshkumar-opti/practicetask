16aee7b7823e3c8cd2d66c218e4773dfb17ca418
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-13 15:55:53 +0530

    feat: fully convert into javascript

--- navsection/script.js
+++ navsection/script.js
@@ -52,15 +52,19 @@ function toggleAccordion(categoryId) {
 }
 
 // Initialize event listeners and handle DOM content
-document.addEventListener("DOMContentLoaded", function () {
+ function handleTransitions () {
   const shopItem = document.querySelector("#shop-desktop");
   const subMenu = document.querySelector("#shop-desktop-dropdown");
-  const mainCategories = document.querySelectorAll("#test-5-main-categories-list > li");
+  const mainCategories = document.querySelectorAll(
+    "#test-5-main-categories-list > li"
+  );
   const subCategories = document.querySelector("#test-5-sub-categories-list");
   const humburger = document.querySelector(".test-5-menu-bars");
   const humburgerClose = document.querySelector(".test-5-menu-close");
   const subNavBar = document.querySelector("#test-5-sub-navbar");
-  const subMenuAccordianLists = document.querySelectorAll(".test-5-mobile-sub-catagory");
+  const subMenuAccordianLists = document.querySelectorAll(
+    ".test-5-mobile-sub-catagory"
+  );
   const shopBack = document.querySelector("#test-5-shop-back");
 
   // Show sub-menu on mouseover for desktop
@@ -190,9 +194,264 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   window.onresize = reportWindowSize;
-});
+};
 
 // Check if the current window width is below or equal to the specified value
 function checkMobileSize(value) {
   return window.innerWidth <= value;
 }
+
+function createBaseElement() {
+
+  if (!document.querySelector('.test-5-container')) {
+    const maincontainerElement = document.createElement("div");
+    maincontainerElement.className = "test-5-container";
+  
+    const htmlInsert = `
+    <div class="test-5-main-navbar-container">
+              <div class="main-container-1">
+                  <div class="test-5-menu-close">
+                      <span>
+                          &#x2715;
+                      </span>
+                  </div>
+                  <div class="test-5-menu-bars">
+                      <hr />
+                      <hr />
+                      <hr />
+                  </div>
+                  <div>
+                      <img src="./assets/logo.png" class="test-5-logo" alt="Optiphoenix logo" />
+                  </div>
+                  <div class="test-5-search-container">
+                      <input type="text" class="test-5-search-input" placeholder="Search Products" />
+                      <img src="./assets/search.png" class="test-5-search-icon" alt="Search logo" />
+                  </div>
+                  <div class="test-5-icons-container">
+                      <div class="test-5-search-mobile-icon">
+                          <img src="./assets/search_mobile.png" alt="mobile responsive search icon" />
+                      </div>
+                      <div class="test-5-user-icon">
+                          <img src="./assets/user.png" alt="user-profile" />
+                      </div>
+                      <div class="test-5-cart-icon">
+                          <img src="./assets/cart.png" alt="cart" />
+                          <div class="test-5-cart-count">
+                              <p>2</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div id="test-5-sub-navbar" class="test-5-sub-navbar-container">
+              <nav class="test-5-desktop">
+                  <ul class="test-5-desktop-navbar">
+                      <li class="">
+                          <a href="javascript:void(0)"> Home </a>
+                      </li>
+                      <li id="shop-desktop">
+                          <a href="javascript:void(0)">
+                              Shop
+                              <img src="./assets/down_arrow.png" class="down_arrow" />
+                              <img src="./assets/up_arrow.png" class="up_arrow" />
+                          </a>
+                          <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                      </li>
+                      <li>
+                          <a href="javascript:void(0)"> Blog </a>
+                      </li>
+                      <li>
+                          <a href="javascript:void(0)"> About </a>
+                      </li>
+                      <li>
+                          <a href="javascript:void(0)"> Contact </a>
+                      </li>
+                  </ul>
+              </nav>
+          </div>
+          <div id="shop-desktop-dropdown" class="test-5-dropdown-menu">
+              <div class="shop-desktop-dropdown-container">
+                  <div class="test-5-dynamic-cotegories-container">
+                      <div class="test-5-main-categories-container">
+                          <div id="test-5-shop-back" class="test-5-mobile-back-shop">
+                              <img src="./assets/up_arrow.png" width="9" height="6" class="left-arrow" />
+                              <p>
+                                  Shop
+                              </p>
+                          </div>
+                          <ul id="test-5-main-categories-list">
+                              <li data-category="1">
+                                  <a href="javascript:void(0)">
+                                      Electronics
+                                  </a>
+                                  <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                              </li>
+                              <div data-category="1" class="test-5-mobile-sub-catagory">
+                                  <ul>
+                                      <li>
+                                          Smartphones
+                                      </li>
+                                      <li>
+                                          Phone
+                                      </li>
+                                  </ul>
+                              </div>
+                              <li data-category="2">
+                                  <a href="javascript:void(0)">
+                                      Fashion and Accessories
+                                  </a>
+                                  <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                              </li>
+                              <div data-category="2" class="test-5-mobile-sub-catagory">
+                                  <ul>
+                                      <li>
+                                          Tops
+                                      </li>
+                                      <li>
+                                          Women's Dresses
+                                      </li>
+                                      <li>
+                                          Women's Shoes
+                                      </li>
+                                      <li>
+                                          Men's Shirts
+                                      </li>
+                                      <li>
+                                          Men's Shoes
+                                      </li>
+                                      <li>
+                                          Men's Watches
+                                      </li>
+                                      <li>
+                                          Women's Watches
+                                      </li>
+                                      <li>
+                                          Women's Bags
+                                      </li>
+                                      <li>
+                                          Women's Jewellery
+                                      </li>
+                                      <li>
+                                          Sunglasses
+                                      </li>
+                                  </ul>
+                              </div>
+                              <li data-category="3">
+                                  <a href="javascript:void(0)">
+                                      Home and Decor
+                                  </a>
+                                  <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                              </li>
+                              <div data-category="3" class="test-5-mobile-sub-catagory">
+                                  <ul>
+                                      <li>
+                                          Home Decoration
+                                      </li>
+                                      <li>
+                                          Furniture
+                                      </li>
+                                      <li>
+                                          Lighting
+                                      </li>
+                                  </ul>
+                              </div>
+                              <li data-category="4">
+                                  <a href="javascript:void(0)">
+                                      Beauty and Personal Care </a>
+                                  <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                              </li>
+                              <div data-category="4" class="test-5-mobile-sub-catagory">
+                                  <ul>
+                                      <li>
+                                          Fragrances
+                                      </li>
+                                      <li>
+                                          Skincare
+                                      </li>
+                                  </ul>
+                              </div>
+                              <li data-category="5">
+                                  <a href="javascript:void(0)">
+                                      Groceries and Automotive
+                                  </a>
+                                  <img src="./assets/up_arrow.png" width="9" height="6" class="right-arrow" />
+                              </li>
+                              <div data-category="5" class="test-5-mobile-sub-catagory">
+                                  <ul>
+                                      <li>
+                                          Groceries
+                                      </li>
+                                      <li>
+                                          Automotive
+                                      </li>
+                                      <li>
+                                          Motorcycle
+                                      </li>
+                                  </ul>
+                              </div>
+                          </ul>
+                      </div>
+                      <div class="test-5-sub-categories-container">
+                          <ul id="test-5-sub-categories-list">
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="test-5-other-pages">
+                      <ul>
+                          <li>
+                              <a href="javascript:void(0)"> Collection Grid </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Simple Product </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Variable Product </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> External Product </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Grouped Product </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> On Sale </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Out of Stock </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Shopping Cart </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Checkout </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Order Complete </a>
+                          </li>
+                          <li>
+                              <a href="javascript:void(0)"> Order Tracking </a>
+                          </li>
+      
+                      </ul>
+                  </div>
+                  <div class="test-5-poster-container">
+                      <div class="test-5-image-container">
+                          <img src="./assets/static_poster.png" 
+                              alt="modern static poster" />
+                          <div class="test-5-poster-content">
+                              <p class="test-5-new-arrivals-text">new arrivals</p>
+                              <a href="javascript:void(0)"> view all </a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
+    maincontainerElement.innerHTML = htmlInsert;
+    document.body.appendChild(maincontainerElement);
+  
+  
+    handleTransitions();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", createBaseElement);
