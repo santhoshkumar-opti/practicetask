1df68fa89eef12b6b7e7dab39cb7022452893726
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-13 15:23:23 +0530

    fix: css style issues

--- navsection/script.js
+++ navsection/script.js
@@ -1,3 +1,4 @@
+// Define the submenu categories
 const subMenuList = {
   1: ["Smartphones", "Phone"],
   2: [
@@ -17,73 +18,54 @@ const subMenuList = {
   5: ["Groceries", "Automotive", "Motorcycle"],
 };
 
+// Define mobile responsive breakpoint
 const mobileResponsive = 1010;
+
+// Create a list item with a text and append it to the parent
 function createListItem(parent, text) {
-  // Create a new list item element
   const li = document.createElement("li");
-
-  // Create a new anchor element
   const a = document.createElement("a");
-
-  // Set the text for the anchor element
   a.textContent = text;
-
-  // Optionally, set the href attribute for the anchor element
-  a.href = "#"; // You can modify this if needed
-
-  // Append the anchor element to the list item
+  a.href = "javascript:void(0)"; // Modify this if needed
   li.appendChild(a);
-
-  // Append the list item to the parent element
   parent.appendChild(li);
 }
 
-// Function to toggle accordion sections
+// Toggle the visibility of accordion sections based on category ID
 function toggleAccordion(categoryId) {
   const allContents = document.querySelectorAll(".test-5-mobile-sub-catagory");
-
   const currentOpenElement = Array.from(allContents).find(
     (category) => category.getAttribute("data-category") == categoryId
   );
 
-  if (!currentOpenElement) {
-    return;
-  }
+  if (!currentOpenElement) return;
 
-  // Check if the clicked section is already open
   const isCurrentlyOpen = currentOpenElement.style.display === "block";
 
-  // Close all accordion items
   allContents.forEach((item) => {
     item.style.display = "none";
   });
 
-  // Toggle the clicked section if it was not already open
   if (!isCurrentlyOpen) {
     currentOpenElement.style.display = "block";
   }
 }
 
+// Initialize event listeners and handle DOM content
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all menu items
   const shopItem = document.querySelector("#shop-desktop");
   const subMenu = document.querySelector("#shop-desktop-dropdown");
-  const mainCategories = document.querySelectorAll(
-    "#test-5-main-categories-list > li:nth-child(n)"
-  );
+  const mainCategories = document.querySelectorAll("#test-5-main-categories-list > li");
   const subCategories = document.querySelector("#test-5-sub-categories-list");
   const humburger = document.querySelector(".test-5-menu-bars");
   const humburgerClose = document.querySelector(".test-5-menu-close");
   const subNavBar = document.querySelector("#test-5-sub-navbar");
-  const subMenuAccordianLists = document.querySelectorAll(
-    ".test-5-mobile-sub-catagory"
-  );
+  const subMenuAccordianLists = document.querySelectorAll(".test-5-mobile-sub-catagory");
   const shopBack = document.querySelector("#test-5-shop-back");
-  // Add event listeners to show sub-menu on mouseover
+
+  // Show sub-menu on mouseover for desktop
   shopItem.addEventListener("mouseover", function () {
-    if (checkMobileSize(mobileResponsive)) {
-      return;
-    }
+    if (checkMobileSize(mobileResponsive)) return;
 
     shopItem.classList.add("hover-active");
     if (subMenu) {
@@ -92,26 +74,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Add click event listner to navigator sub catagories
+  // Show mobile sub-menu on click
   shopItem.addEventListener("click", function () {
-    if (!checkMobileSize(mobileResponsive)) {
-      return;
-    }
+    if (!checkMobileSize(mobileResponsive)) return;
 
     if (subMenu) {
       subMenu.classList.add("test-5-dropdown-menu-mobile");
     }
   });
 
+  // Go back to the previous menu in mobile view
   shopBack.addEventListener("click", () => {
     resetSubcatagoryAndRemoveClass();
   });
 
-  // Add event listener to hide sub-menu on mouseout
+  // Hide sub-menu on mouseout for desktop
   shopItem.addEventListener("mouseleave", function (event) {
-    if (checkMobileSize(mobileResponsive)) {
-      return;
-    }
+    if (checkMobileSize(mobileResponsive)) return;
+
     if (!subMenu.contains(event.toElement)) {
       shopItem.classList.remove("hover-active");
       if (subMenu) {
@@ -121,11 +101,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Add event listener to hide sub-menu on mouseleave
+  // Hide sub-menu on mouseleave for desktop
   subMenu.addEventListener("mouseleave", function () {
-    if (checkMobileSize(mobileResponsive)) {
-      return;
-    }
+    if (checkMobileSize(mobileResponsive)) return;
 
     shopItem.classList.remove("hover-active");
     if (subMenu) {
@@ -134,83 +112,78 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // Display subcategories on mouseover
   mainCategories.forEach((category) => {
     category.addEventListener("mouseover", () => {
       subCategories.innerHTML = "";
 
-      if (checkMobileSize(mobileResponsive)) {
-        return;
-      }
+      if (checkMobileSize(mobileResponsive)) return;
 
       const categoryId = category.getAttribute("data-category");
-
       const lists = subMenuList[categoryId];
       lists.forEach((list) => createListItem(subCategories, list));
     });
 
+    // Toggle accordion on click for mobile
     category.addEventListener("click", () => {
-      if (!checkMobileSize(mobileResponsive)) {
-        return;
-      }
+      if (!checkMobileSize(mobileResponsive)) return;
 
       const categoryId = category.getAttribute("data-category");
-
       toggleAccordion(categoryId);
     });
   });
 
-  // Hide subcategories when mouse leaves the main categories area
+  // Hide subcategories when mouse leaves the categories area
   document
     .querySelector(".test-5-dynamic-cotegories-container")
     .addEventListener("mouseleave", () => {
-      if (checkMobileSize(mobileResponsive)) {
-        return;
-      }
+      if (checkMobileSize(mobileResponsive)) return;
 
       subCategories.innerHTML = "";
     });
 
-
-
+  // Show mobile menu
   humburger.addEventListener("click", () => {
     humburgerClose.style.display = "flex";
     document.body.classList.add("test-5-mobile-body");
     subNavBar.classList.add("test-5-sub-navbar-container-mobile");
   });
+
+  // Close mobile menu
   humburgerClose.addEventListener("click", () => {
-    resetEntireMobileResponsive()
+    resetEntireMobileResponsive();
   });
 
+  // Reset mobile menu and submenu
   function resetEntireMobileResponsive() {
     humburgerClose.style.display = "none";
     document.body.classList.remove("test-5-mobile-body");
     subNavBar.classList.remove("test-5-sub-navbar-container-mobile");
-
     resetSubcatagoryAndRemoveClass();
   }
 
+  // Reset submenu
   function resetSubcatagoryAndRemoveClass() {
-    // close all the sub catagory
     subMenuAccordianLists.forEach((item) => {
       item.style.display = "none";
     });
-
-    // hide the submenu
-    subMenu.classList.remove("test-5-dropdown-menu-mobile");
+    if (subMenu) {
+      subMenu.classList.remove("test-5-dropdown-menu-mobile");
+    }
   }
 
+  // Handle window resize to adjust menu display
   function reportWindowSize() {
-    // from desktop to mobile responsive
-    if (shopItem && subMenu && window.innerWidth <= 1010) {
-      if (shopItem.className.includes("hover-active")) {
+    if (shopItem && subMenu && window.innerWidth <= mobileResponsive) {
+      if (shopItem.classList.contains("hover-active")) {
         shopItem.classList.remove("hover-active");
         subMenu.style.display = "none";
         subMenu.style.height = "0px";
       }
     }
 
-    if (window.innerWidth > 1010) {
-      if (document.body.className.includes("test-5-mobile-body")) {
+    if (window.innerWidth > mobileResponsive) {
+      if (document.body.classList.contains("test-5-mobile-body")) {
         resetEntireMobileResponsive();
       }
     }
@@ -219,6 +192,7 @@ document.addEventListener("DOMContentLoaded", function () {
   window.onresize = reportWindowSize;
 });
 
+// Check if the current window width is below or equal to the specified value
 function checkMobileSize(value) {
   return window.innerWidth <= value;
 }
