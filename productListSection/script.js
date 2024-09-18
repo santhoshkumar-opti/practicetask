ce37fcb4d532fa829853f6f87aaee61959625f78
santhoshkumar <santhoshkumar.viswanthan@optiphoenix.com>
2024-09-18 12:38:20 +0530

    feat: images added

--- productListSection/script.js
+++ productListSection/script.js
@@ -14,3 +14,18 @@ tabs.forEach((tab) => {
     tab.scrollIntoView({ behavior: "smooth", inline: "center" });
   });
 });
+
+
+
+const imagesHolders = {
+  favourite: "https://i.postimg.cc/rm3MMhLf/favourit.png",
+  item1: "https://i.postimg.cc/Y2Ltbw1Z/item-1-web.png",
+  item2: "https://i.postimg.cc/xTrnZF5C/item-2-web.png",
+  item3: "https://i.postimg.cc/Kvnxhrp5/item-3-web.png",
+  item4: "https://i.postimg.cc/8zNDpJqg/item-4-web.png",
+  item5: "https://i.postimg.cc/8zNDpJqg/item-5-web.png",
+  item6: "https://i.postimg.cc/8zNDpJqg/item-6-web.png",
+  item7: "https://i.postimg.cc/8zNDpJqg/item-7-web.png",
+  item8: "https://i.postimg.cc/8zNDpJqg/item-8-web.png",
+
+}
